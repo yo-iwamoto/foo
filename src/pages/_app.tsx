16c0c4da59702460bb4384159c57e9203243cf6a
you-5805 <you.iwamoto918@gmail.com>
2021-04-25 23:08:09 +0900

    change: meta

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,7 +8,7 @@ const App = ({ Component, pageProps }: AppProps) => {
     <>
       <Head>
         <title>Foo ひとりご飯の検索サイト</title>
-        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+        <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0" />
         <link rel="icon" href="favicon.ico"/>
         <link rel="apple-touch-icon" href="images/apple-touch-icon.png" sizes="180x180" />
         <link rel="android-touch-icon" href="images/android-touch-icon.png" sizes="192x192" />
