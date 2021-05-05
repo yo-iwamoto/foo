8dc933b44a595b7902a2449d3363ee5dd35cfe34
you-5805 <you.iwamoto918@gmail.com>
2021-05-06 00:03:40 +0900

    add prefix NEXT_PUBLIC

--- src/api/restaurants.ts
+++ src/api/restaurants.ts
@@ -2,8 +2,8 @@ import axios from 'axios';
 import { Shop, HotpepperResponse } from '../types';
 import generalSlice from '../ducks/general/slice';
 
-const key = process.env.API_KEY,
-      production = process.env.NODE_ENV === 'production';
+const key = process.env.NEXT_PUBLIC_API_KEY;
+const production = process.env.NODE_ENV === 'production';
 
 let baseUrl = `/hotpepper/gourmet/v1`;
 if (production) {
