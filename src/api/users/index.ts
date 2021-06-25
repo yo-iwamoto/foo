42ab48742b3f4b135d29d107f60729068c58692d
you-5805 <you.iwamoto918@gmail.com>
2021-06-26 04:13:45 +0900

    fix: API

--- src/api/users/index.ts
+++ src/api/users/index.ts
@@ -1,11 +1,13 @@
 import { axios } from '@/api/lib/axios';
 import { AxiosResponse } from 'axios';
 import { FooSignInResource, UpdateNameResource, UserResponse } from '@/types';
+import { FooEndPoint } from '../lib/url';
 
 export class UsersController {
   static signUp = async (resource: FooSignInResource): Promise<UserResponse> => {
+    const endPoint = new FooEndPoint('/users', resource);
     try {
-      const res = await axios.post('/users', resource);
+      const res = await axios.post(endPoint.url);
       localStorage.setItem('Access-Token', res.headers['access-token']);
       return res.data;
     } catch (err) {
@@ -14,8 +16,9 @@ export class UsersController {
   };
 
   static logIn = async (resource: FooSignInResource): Promise<UserResponse> => {
+    const endPoint = new FooEndPoint('/sessions', resource);
     try {
-      const res = await axios.post('/sessions', resource);
+      const res = await axios.post(endPoint.url);
       localStorage.setItem('Access-Token', res.headers['access-token']);
       return res.data;
     } catch (err) {
@@ -24,8 +27,9 @@ export class UsersController {
   };
 
   static autoLogIn = async (): Promise<UserResponse> => {
+    const endPoint = new FooEndPoint('/sessions');
     try {
-      const res: AxiosResponse<UserResponse> = await axios.post('/sessions');
+      const res: AxiosResponse<UserResponse> = await axios.post(endPoint.url);
       return res.data;
     } catch (err) {
       throw err;
@@ -33,11 +37,11 @@ export class UsersController {
   };
 
   static updateName = async (resource: UpdateNameResource): Promise<UserResponse> => {
+    const endPoint = new FooEndPoint(`/users/${resource.uid}`, {
+      name: resource.name,
+    });
     try {
-      const params = {
-        name: resource.name,
-      };
-      const res: AxiosResponse<UserResponse> = await axios.patch(`/users/${resource.uid}`, params);
+      const res: AxiosResponse<UserResponse> = await axios.patch(endPoint.url);
       return res.data;
     } catch (err) {
       throw err;
